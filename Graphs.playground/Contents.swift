ae4572bf6132c8a7260acdc07f1a8bb9dcf164e0
Richard Robinson <kincade71@gmail.com>
2020-11-29 18:08:13 -0500

    Create Contents.swift

---
+++ Graphs.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: A UIKit based Playground for presenting user interface
+  
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.setLiveView(ContentView())
